c4b396d17d6ae3b9efccda358d40ee9a2198688d
Vladislav Stefanov <vladislav.stefanov@news.co.uk>
2024-10-30 14:57:36 +0200

    remoce account id from script

--- scripts/ecr-push.sh
+++ scripts/ecr-push.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 ENVIRONMENT=$1
+ACCOUNT_ID=$2
 
-aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 504565868142.dkr.ecr.eu-west-1.amazonaws.com
+aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin ${ACCOUNT_ID}.dkr.ecr.eu-west-1.amazonaws.com
 
 docker build -t mbition-kutt-${ENVIRONMENT} .
 
-docker tag mbition-kutt-${ENVIRONMENT}:latest 504565868142.dkr.ecr.eu-west-1.amazonaws.com/mbition-kutt-${ENVIRONMENT}:latest
+docker tag mbition-kutt-${ENVIRONMENT}:latest ${ACCOUNT_ID}.dkr.ecr.eu-west-1.amazonaws.com/mbition-kutt-${ENVIRONMENT}:latest
 
-docker push 504565868142.dkr.ecr.eu-west-1.amazonaws.com/mbition-kutt-${ENVIRONMENT}:latest
+docker push ${ACCOUNT_ID}.dkr.ecr.eu-west-1.amazonaws.com/mbition-kutt-${ENVIRONMENT}:latest
